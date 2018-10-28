d69c4928fdb3581e9a63975ba5cb44297dcb838b
alexbanshchikov <banshchikov.alex@outlook.com>
2018-10-29 01:37:52 +0700

    Fixed Calculate method.

--- TaxiSOS/TaxiSOS/Services/OrderService.cs
+++ TaxiSOS/TaxiSOS/Services/OrderService.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GMap.NET;
-using GMap.NET.MapProviders;
-using GMap.NET.WindowsForms;
-using GMap.NET.WindowsForms.Markers;
-using GMap.NET.WindowsForms.ToolTips;
-using System.Drawing;
+﻿using System.Linq;
 using Newtonsoft.Json.Linq;
 using System.Net;
-using System.IO;
+using System.Globalization;
 
 namespace TaxiSOS.Services
 {
@@ -18,23 +9,22 @@ namespace TaxiSOS.Services
     {
         public int Calculate(string From, string To)
         {
-
-            string responsetext = new WebClient().DownloadString("http://search.maps.sputnik.ru/search?q=" + From);
-            var rootFrom = JObject.Parse(responsetext);
+            string responseFrom = new WebClient().DownloadString("http://search.maps.sputnik.ru/search?q=" + From);
+            var rootFrom = JObject.Parse(responseFrom);
             var latFrom =
                     rootFrom.DescendantsAndSelf()
                         .OfType<JProperty>()
                         .Where(p => p.Name == "lat")
                         .Select(p => p.Value).First();
-            var l = latFrom.Values(0);
+
             var lonFrom =
                     rootFrom.DescendantsAndSelf()
                         .OfType<JProperty>()
                         .Where(p => p.Name == "lon")
                         .Select(p => p.Value).First();
 
-            string responsetextTo = new WebClient().DownloadString("http://search.maps.sputnik.ru/search?q=" + To);
-            var rootTo = JObject.Parse(responsetext);
+            string responseTo = new WebClient().DownloadString("http://search.maps.sputnik.ru/search?q=" + To);
+            var rootTo = JObject.Parse(responseTo);
             var latTo =
                     rootTo.DescendantsAndSelf()
                         .OfType<JProperty>()
@@ -46,17 +36,21 @@ namespace TaxiSOS.Services
                         .OfType<JProperty>()
                         .Where(p => p.Name == "lon")
                         .Select(p => p.Value).First();
-            string route = new WebClient().DownloadString("http://routes.maps.sputnik.ru/osrm/router/viaroute?loc="+ latFrom.ToString()+ ","+ lonFrom.ToString() + "&"+"loc="+ latTo.ToString() + ","+ lonTo.ToString() + "&instructions=true");
 
+            string route = new WebClient().DownloadString("http://routes.maps.sputnik.ru/osrm/router/viaroute?loc="
+                +latFrom.Value<float>().ToString(CultureInfo.GetCultureInfo("en-US")) +","
+                +lonFrom.Value<float>().ToString(CultureInfo.GetCultureInfo("en-US")) +"&loc="
+                +latTo.Value<float>().ToString(CultureInfo.GetCultureInfo("en-US")) +","
+                +lonTo.Value<float>().ToString(CultureInfo.GetCultureInfo("en-US")));
+            
             var root = JObject.Parse(route);
             var distance =
                     root.DescendantsAndSelf()
                         .OfType<JProperty>()
                         .Where(p => p.Name == "total_distance")
-                        .Select(p => p.Value);
+                        .Select(p => p.Value).First();
 
-            // route.Stroke = new Pen(Color.Red, 3); //Задаем цвет и ширину линии
-            return 0;
+            return distance.Value<int>();
         }
     }
 }
