38034c4eb2e324b93ed6f243358a6704fad87027
alexbanshchikov <banshchikov.alex@outlook.com>
2018-11-11 20:00:06 +0700

    Added StatusEnumerator.

---
+++ TaxiSOS/DataModel/Enums/StatusEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Enums
+{
+    public class StatusEnum
+    {
+        enum Status
+        {
+            WithoutDriver = 1,
+            DriverOnWay,
+            AwaitingClient,
+            ClientOnWay,
+            Success
+        }
+    }
+}
