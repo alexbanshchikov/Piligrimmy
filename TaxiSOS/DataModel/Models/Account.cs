133fc66b83ce9c15a1c08a610617a8d960a5f157
alexbanshchikov <banshchikov.alex@outlook.com>
2019-01-05 22:05:03 +0700

    Added new Account model.

---
+++ TaxiSOS/DataModel/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataModel.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            Clients = new HashSet<Clients>();
+            Drivers = new HashSet<Drivers>();
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public virtual ICollection<Clients> Clients { get; set; }
+        public virtual ICollection<Drivers> Drivers { get; set; }
+    }
+}
