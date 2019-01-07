8c7d7df3f2e0c1f39f6f3204159d701f5036271e
alexbanshchikov <banshchikov.alex@outlook.com>
2019-01-07 20:38:40 +0700

    Changed token lifetime.

--- TaxiSOS/TaxiSOS/Services/AuthOptions.cs
+++ TaxiSOS/TaxiSOS/Services/AuthOptions.cs
@@ -12,7 +12,7 @@ namespace TaxiSOS.Services
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "http://localhost:8080/"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 1; // время жизни токена - 1 минута
+        public const int LIFETIME = 30; // время жизни токена - 1 минута
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
