4271328ca1155eaee32d2b2a96d94216dd5026f6
alexbanshchikov <banshchikov.alex@outlook.com>
2019-01-12 20:47:52 +0700

    Added Account Form.

---
+++ TaxiSOS/DesktopClient/FormAccount.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DesktopClient
+{
+    partial class FormAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMap = new System.Windows.Forms.Button();
+            this.buttonAccount = new System.Windows.Forms.Button();
+            this.checkBoxBusy = new System.Windows.Forms.CheckBox();
+            this.buttonBackToMap = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonMap
+            // 
+            this.buttonMap.Location = new System.Drawing.Point(12, 5);
+            this.buttonMap.Name = "buttonMap";
+            this.buttonMap.Size = new System.Drawing.Size(75, 40);
+            this.buttonMap.TabIndex = 3;
+            this.buttonMap.Text = "Главная";
+            this.buttonMap.UseVisualStyleBackColor = true;
+            // 
+            // buttonAccount
+            // 
+            this.buttonAccount.Location = new System.Drawing.Point(155, 5);
+            this.buttonAccount.Name = "buttonAccount";
+            this.buttonAccount.Size = new System.Drawing.Size(75, 40);
+            this.buttonAccount.TabIndex = 4;
+            this.buttonAccount.Text = "Личный счёт";
+            this.buttonAccount.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBusy
+            // 
+            this.checkBoxBusy.AutoSize = true;
+            this.checkBoxBusy.Location = new System.Drawing.Point(290, 18);
+            this.checkBoxBusy.Name = "checkBoxBusy";
+            this.checkBoxBusy.Size = new System.Drawing.Size(56, 17);
+            this.checkBoxBusy.TabIndex = 5;
+            this.checkBoxBusy.Text = "Занят";
+            this.checkBoxBusy.UseVisualStyleBackColor = true;
+            // 
+            // buttonBackToMap
+            // 
+            this.buttonBackToMap.Location = new System.Drawing.Point(12, 470);
+            this.buttonBackToMap.Name = "buttonBackToMap";
+            this.buttonBackToMap.Size = new System.Drawing.Size(360, 80);
+            this.buttonBackToMap.TabIndex = 8;
+            this.buttonBackToMap.Text = "Вернуться";
+            this.buttonBackToMap.UseVisualStyleBackColor = true;
+            this.buttonBackToMap.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(359, 412);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // FormAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 562);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBackToMap);
+            this.Controls.Add(this.checkBoxBusy);
+            this.Controls.Add(this.buttonAccount);
+            this.Controls.Add(this.buttonMap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormAccount";
+            this.Text = "FormAccount";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMap;
+        private System.Windows.Forms.Button buttonAccount;
+        private System.Windows.Forms.CheckBox checkBoxBusy;
+        private System.Windows.Forms.Button buttonBackToMap;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
--- TaxiSOS/DesktopClient/FormAccount.cs
+++ TaxiSOS/DesktopClient/FormAccount.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DesktopClient
 {
-    public partial class Form1 : Form
+    public partial class FormAccount : Form
     {
-        public Form1()
+        public FormAccount()
         {
             InitializeComponent();
         }
