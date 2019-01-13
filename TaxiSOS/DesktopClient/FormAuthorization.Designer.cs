24ce6c1762fff0af0b3a5e44c41b96db4547a03c
alexbanshchikov <banshchikov.alex@outlook.com>
2019-01-13 22:54:35 +0700

    Added Authorization Form.

---
+++ TaxiSOS/DesktopClient/FormAuthorization.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DesktopClient
+{
+    partial class FormAuthorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonAuthorize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(117, 230);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(153, 20);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(117, 286);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(153, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(114, 214);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(38, 13);
+            this.labelLogin.TabIndex = 2;
+            this.labelLogin.Text = "Логин";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(114, 270);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(45, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // buttonAuthorize
+            // 
+            this.buttonAuthorize.Location = new System.Drawing.Point(152, 326);
+            this.buttonAuthorize.Name = "buttonAuthorize";
+            this.buttonAuthorize.Size = new System.Drawing.Size(75, 23);
+            this.buttonAuthorize.TabIndex = 4;
+            this.buttonAuthorize.Text = "Войти";
+            this.buttonAuthorize.UseVisualStyleBackColor = true;
+            this.buttonAuthorize.Click += new System.EventHandler(this.buttonAuthorize_Click);
+            // 
+            // FormAuthorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 562);
+            this.Controls.Add(this.buttonAuthorize);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormAuthorization";
+            this.Text = "FormAuthorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonAuthorize;
+    }
+}
---
+++ TaxiSOS/DesktopClient/FormAuthorization.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataModel;
+
+namespace DesktopClient
+{
+    public partial class FormAuthorization : Form
+    {
+        public FormAuthorization()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAuthorize_Click(object sender, EventArgs e)
+        {
+            Account account = new Account(); //TODO Переделать DataModel на библиотеку классов .NET Standart
+
+            string username = textBoxLogin.Text;
+            string password = textBoxPassword.Text;
+
+            //WebRequest request = WebRequest.Create("http://localhost:8080/token");
+            //WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+            //    + "key=trnsl.1.1.20170125T084253Z.cc366274cc3474e9.68d49c802348b39b5d677c856e0805c433b7618c"//Ключ
+            //    + "&text=" + s//Текст
+            //    + "&lang=" + language);//Язык
+
+
+
+            //Получаем ответ
+            //WebResponse response = request.GetResponse();
+            //--------------------
+            //---Распарсить JSON ответ. Я скачал фреймворк Json.NET
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                string line;
+                if ((line = stream.ReadLine()) != null)
+                {
+                    //Translation translation = JsonConvert.DeserializeObject<Translation>(line);
+                    //s = "";
+                    //foreach (string str in translation.text)
+                    //{
+                    //    s += str;
+                    //}
+                }
+            }
+            //------------------
+        }
+
+        /*
+         var tokenKey = "accessToken";
+var clientKey = "id_Client";
+$('#submitLogin').click(function (e) {
+    e.preventDefault();
+    var loginData = {
+        grant_type: 'password',
+        username: $('#emailLogin').val(),
+        password: $('#passwordLogin').val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/token',
+        data: loginData
+    }).success(function (data) {
+        $('.userName').text(data.username);
+        $('.id_client').text(data.id_Client);
+
+        sessionStorage.setItem(tokenKey, data.access_token);
+        sessionStorage.setItem(clientKey, data.id_Client);
+        window.location.href = "/Index.html";
+    }).fail(function (data) {
+        console.log(data);
+    });
+});
+         */
+    }
+}
